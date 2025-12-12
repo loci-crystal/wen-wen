7ffbb80015e169f118d7b0c993dda0e451a0ded3
wenwen <41246829+loci-crystal@users.noreply.github.com>
2025-12-12 20:22:38 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建游戏类
+            Game game = new Game();
+            //运行
+            game.Start();
+        }
+    }
+}
